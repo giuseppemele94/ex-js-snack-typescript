e70f95aff01dc8c04e01859b2a9f0d0a9c40b3db
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-04-27 16:53:05 +0200

    inizializzazione repo + snack1

---
+++ src/main.ts
@@ -0,0 +1,44 @@
+/* SNACK 1* 
+Hai ricevuto un dato generico da un'API, ma non sai di che tipo sia… 
+Il tuo compito è controllare il tipo del dato e stampare il valore in modo corretto.
+
+- Se è una stringa: stampala in maiuscolo
+- Se è un numero: moltiplicalo per due e stampalo
+- Se è un booleano: stampa “Sì” o “No” in base al suo valore
+- In tutti gli altri casi: stampa “Tipo non supportato”
+
+ BONUS
+Se è null: stampa “Il dato è vuoto”
+Se è un array: stampa la sua lunghezza
+Se è una Promise: attendi che si risolva e stampa il valore del resolve.
+
+*/
+
+let datoGenerico: unknown ;
+
+if(datoGenerico === null) {
+  console.log("Il dato è vuoto");
+}
+else if(Array.isArray(datoGenerico)) {
+  console.log(datoGenerico.length);
+}
+
+else if(datoGenerico instanceof Promise) {
+  const valoreRisolto = await datoGenerico;
+  console.log(valoreRisolto);
+}
+
+else if(typeof datoGenerico === "string") {
+  const datoInMaiuscolo = datoGenerico.toUpperCase(); 
+  console.log(datoInMaiuscolo); 
+}
+else if(typeof datoGenerico === "number") {
+  const risultato = datoGenerico * 2;
+  console.log(risultato);
+}
+else if(typeof datoGenerico === "boolean") {
+  console.log(datoGenerico ? "Si" : "No"); 
+}
+else {
+  console.log("Tipo non supportato"); 
+}
